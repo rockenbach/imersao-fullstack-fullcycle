ee6cda202bfb434331404518ca00ff637db10b63
Charles Rockenbach <charlesrockenbach@gmail.com>
2021-02-02 09:03:34 -0300

    Add tests

--- codepix/domain/model/base.go
+++ codepix/domain/model/base.go
@@ -1,8 +1,8 @@
 package model
 
 import (
-	"time",
 	"github.com/asaskevich/govalidator"
+	"time"
 )
 
 func init() {
@@ -10,7 +10,7 @@ func init() {
 }
 
 type Base struct {
-	ID        string    `json:"id" valid:"uuid"` 
+	ID        string    `json:"id" valid:"uuid"`
 	CreatedAt time.Time `json:"created_at" valid:"-"`
 	UpdatedAt time.Time `json:"updated_at" valid:"-"`
 }
